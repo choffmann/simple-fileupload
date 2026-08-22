a004068c645221ef4ea7106a367022c4fb3457a3
Cedrik Hoffmann <dev@choffmann.io>
2026-08-22 14:22:07 +0200

    chore: add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Development enviroment for go";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs = inputs: let
+    goVersion = 26;
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [inputs.self.overlays.default];
+          };
+        });
+  in {
+    overlays.default = final: prev: {
+      go = final."go_1_${toString goVersion}";
+    };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          gotools
+          golangci-lint
+
+          gnumake
+          pkg-config
+          yq-go
+        ];
+      };
+    });
+  };
+}
